3b098d9ba3f1dbeb75438a5ef4a68faae5f6de26
Marcell <Marcell@notebook-dell>
2017-01-13 23:49:42 -0200

    Tela de Login criada

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,63 +1,36 @@
 <!DOCTYPE html>
 <html>
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>ON TECH | Login</title>
-
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-</head>
-
+    <head>
+        @include('templates.top')
+        <title>ON TECH | Login</title>
+    </head>
 <body class="gray-bg">
-
-
-
     <div class="middle-box text-center loginscreen animated fadeInDown">
         <div>
-        
-        
-        
           <div>
-
                 <h1 class="logo-name">ON</h1>
-
             </div>
             <h3>Bem Vindo!</h3>
             <p>On Tech é um sistema para gerenciamento de chamados.<!--Continually expanded and constantly improved Inspinia Admin Them (IN+)--></p>
             <p>&nbsp;</p>
-            
-            
-            
-            
             <form class="m-t" role="form" action="index.html">
                 <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Usuario" name="email" required>
+                    <input type="email" class="form-control" placeholder="E-mail" name="usuario" required>
                 </div>
                 <div class="form-group">
                     <input type="password" class="form-control" placeholder="Senha" name="senha" required>
                 </div>
                 <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-                <a href="#"><small>Esqueceu a senha?</small></a>
-                <p class="text-muted text-center"><small>Não tem uma conta ainda?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="register.html">Criar uma conta</a>
+                <a href="#"><small>Esqueceu a senha?</small></a> //TODO
+<!--                <p class="text-muted text-center"><small>Não tem uma conta ainda?</small></p>-->
+<!--                <a class="btn btn-sm btn-white btn-block" href="register.html">Criar uma conta</a>-->
             </form>
             <p class="m-t">&nbsp;</p>
         </div>
     </div>
 
-    <!-- Mainly scripts -->
-    <script src="js/jquery-2.1.1.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    @include('templates.scripts')
 
 </body>
-
 </html>
---
+++ resources/views/templates/scripts.blade.php
@@ -0,0 +1,3 @@
+<!-- Mainly scripts -->
+    <script src="{!!asset('js/jquery-2.1.1.js')!!}"></script>
+    <script src="{!!asset('js/bootstrap.min.js')!!}"></script>
